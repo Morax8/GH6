bb9a048c248b12451719974dc5b0be5fd8998990
Aditya Zianur <adityazianurrahman@gmail.com>
2025-07-24 17:43:56 +0700

    adding new routes for explore and games

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,15 +1,19 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="w-full px-6 py-4 flex justify-between items-center bg-white shadow">
       <div className="text-lg font-bold">LOGO</div>
       <ul className="flex space-x-8 text-sm font-medium">
-        <li className="font-bold font hover:text-gray-700 cursor-pointer">
-          Home
+        <li className="font-bold hover:text-gray-700 cursor-pointer">
+          <Link href="/">Home</Link>
         </li>
-        <li className="font-bold  hover:text-gray-700 cursor-pointer">
-          Explore
+        <li className="font-bold hover:text-gray-700 cursor-pointer">
+          <Link href="/explore">Explore</Link>
         </li>
-        <li className="font-bold  hover:text-gray-700 cursor-pointer">Games</li>
+        <li className="font-bold hover:text-gray-700 cursor-pointer">
+          <Link href="/game">Games</Link>
+        </li>
       </ul>
     </nav>
   );
---
+++ app/game/quiz/page.js
@@ -0,0 +1,7 @@
+export default function Game() {
+  return (
+    <div>
+      <h1>Game Component</h1>
+    </div>
+  );
+}
