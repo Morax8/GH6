32536e982e89a75938d3c812fd4ab0e15191e1e9
Aditya Zianur <adityazianurrahman@gmail.com>
2025-07-25 01:58:11 +0700

    marquee for cariusel 6th contennt

---
+++ app/components/MarqueeData.js
@@ -0,0 +1,47 @@
+const cardData = [
+  {
+    id: 1,
+    image: "/images/bajuAdat.png",
+    title: "Judul 1",
+    desc: "Deskripsi 1",
+    hasOverlay: true,
+  },
+  {
+    id: 2,
+    image: "/images/bali.png",
+    title: "Judul 2",
+    desc: "Deskripsi 2",
+    hasOverlay: true,
+  },
+  {
+    id: 3,
+    image: "/images/adat2.png",
+    title: "Lorem Ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquet laoreet dolor.",
+    hasOverlay: true,
+  },
+  {
+    id: 4,
+    image: "/images/kudaLumping.png",
+    title: "Judul 4",
+    desc: "Deskripsi 4",
+    hasOverlay: true,
+  },
+  {
+    id: 5,
+    image: "/images/wayang.png",
+    title: "Judul 5",
+    desc: "Deskripsi 5",
+    hasOverlay: true,
+  },
+  {
+    id: 6,
+    image: "/images/reog.png",
+    title: "Judul 6",
+    desc: "Deskripsi 6",
+    hasOverlay: true,
+  },
+  // dst...
+];
+
+export default cardData;
