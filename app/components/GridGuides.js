64de6a4be46a6c9f5f4ce86e56d9dbce3a9419c4
Aditya Zianur <adityazianurrahman@gmail.com>
2025-07-24 17:00:08 +0700

    fixing width

--- app/components/GridGuides.js
+++ app/components/GridGuides.js
@@ -2,27 +2,19 @@
 
 import React from "react";
 
-const GridGuides = () => {
-  const columns = 12;
-  const gutter = 16; // px
-  const columnWidth = `calc((100% - ${(columns - 1) * gutter}px) / ${columns})`;
+export default function GridGuides() {
+  const gridsGuide = new Array(12).fill(null);
 
   return (
     <div
-      className="pointer-events-none fixed inset-0 z-[9999] hidden md:flex"
+      className="pointer-events-none fixed inset-0 z-[9999] hidden md:block"
       style={{ mixBlendMode: "difference" }}
     >
-      <div className="w-full flex px-4 gap-[16px]">
-        {Array.from({ length: columns }).map((_, i) => (
-          <div
-            key={i}
-            className="bg-white/10 h-full"
-            style={{ width: columnWidth }}
-          />
+      <div className="absolute grid grid-cols-12 px-16 py-4 w-full h-full gap-5">
+        {gridsGuide.map((_, i) => (
+          <div key={i} className="bg-red-200 w-full h-full opacity-10"></div>
         ))}
       </div>
     </div>
   );
-};
-
-export default GridGuides;
+}
