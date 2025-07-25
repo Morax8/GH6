7129411351b291297a0c2e012c275ea00b6be4e6
Aditya Zianur <adityazianurrahman@gmail.com>
2025-07-25 15:30:13 +0700

    adding logo and Hero section text

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation';    
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -8,18 +8,24 @@ export default function Navbar() {
 
   return (
     <nav className="w-full px-32 py-4 flex justify-between items-center bg-white shadow text-black">
-      <div className="text-lg font-bold">LOGO</div>
+      <div className="text-lg font-bold">
+        <img src="/Assets/logo.svg" alt="Logo" className="w-20" />
+      </div>
       <ul className="flex space-x-8 text-sm font-medium">
-        <li className="font-bold font hover:text-gray-700 cursor-pointer"
-          onClick={() => router.push("/")}>
+        <li
+          className="font-bold font hover:text-gray-700 cursor-pointer"
+          onClick={() => router.push("/")}
+        >
           Home
         </li>
-        <li className="font-bold hover:text-gray-700 cursor-pointer"
+        <li
+          className="font-bold hover:text-gray-700 cursor-pointer"
           onClick={() => router.push("/explorePage")}
         >
           Explore
         </li>
-        <li className="font-bold hover:text-gray-700 cursor-pointer"
+        <li
+          className="font-bold hover:text-gray-700 cursor-pointer"
           onClick={() => router.push("/game")}
         >
           Games
